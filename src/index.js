e73b28f7ee6e367b3e08c0ede7a8359864d3cb2d
Atsushi Ishida <dolin@Atsushi-no-MacBook-Air.local>
2018-11-05 22:34:49 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import marked from "marked";
+import React from "react";
+
+const root_el = document.getElementById("root");
+const input_el = document.getElementById("input");
+
+var markdown_text = `
+# 見出し1
+
+リストサンプル
+- アイテム1
+- アイテム2
+- アイテム3
+`;
+
+input_el.addEventListener("keyup", function() {
+  let input_text = input_el.value;
+  let html_formatted = marked(input_text);
+  root_el.innerHTML = html_formatted;
+});
