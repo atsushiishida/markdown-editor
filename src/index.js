cc4cd6e5ce90304b7f9180e9eaa216896ae65726
Atsushi Ishida <dolin@Atsushi-no-MacBook-Air.local>
2018-11-05 23:48:12 +0900

    Enbaled React

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import marked from "marked";
+import React from "react";
+import ReactDOM from "react-dom";
 
 const root_el = document.getElementById("root");
 const input_el = document.getElementById("input");
 
-var markdown_text = `
-# 見出し1
-
-リストサンプル
-- アイテム1
-- アイテム2
-- アイテム3
-`;
-
 input_el.addEventListener("keyup", function() {
   let input_text = input_el.value;
   let html_formatted = marked(input_text);
-  root_el.innerHTML = html_formatted;
+  ReactDOM.render(<MDPreview text_html={html_formatted} />, root_el);
 });
+
+class MDPreview extends React.Component {
+  render() {
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: this.props.text_html
+        }}
+      />
+    );
+  }
+}
